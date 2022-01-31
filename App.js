a37ddd850cf6b40968fa13fe510f00483b311fa3
Alexander Matias Urrutia Salazar <aurrutia.externo@coopeuch.cl>
2022-01-31 16:28:32 -0300

    Test

--- App.js
+++ App.js
@@ -1,51 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import AppLoading from "expo-app-loading";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { NavigationContainer, DarkTheme } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import {
-  useFonts,
-  Poppins_400Regular,
-  Poppins_700Bold,
-} from "@expo-google-fonts/poppins";
-import { Provider } from "react-redux";
-import { Store } from "./redux/store";
-
-//Screens
-import HomeScreen from "./screens/HomeScreen";
-import PokemonScreen from "./screens/PokemonScreen";
 
 const App = () => {
-  const Stack = createStackNavigator();
-  let [poppinsLoaded] = useFonts({
-    Poppins_400Regular,
-    Poppins_700Bold,
-  });
-
-  if (!poppinsLoaded) {
-    return <AppLoading />;
-  } else {
-    return (
-      <Provider store={Store}>
-        <NavigationContainer theme={DarkTheme}>
-          <SafeAreaProvider>
-            <Stack.Navigator>
-              <Stack.Screen
-                name="HomeScreen"
-                component={HomeScreen}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="PokemonScreen"
-                component={PokemonScreen}
-                options={{ headerShown: false }}
-              />
-            </Stack.Navigator>
-          </SafeAreaProvider>
-        </NavigationContainer>
-      </Provider>
-    );
-  }
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
 };
 
 export default App;
+
+const styles = StyleSheet.create({});
